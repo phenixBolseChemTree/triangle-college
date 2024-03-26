6943c29309869121ecc52fd5e9f3f646fb7d5d85
Ivan Sharapov <oldrum75@gmail.com>
2024-03-26 16:10:32 +0100

    fix: result bug

--- src/components/TriangleV2.jsx
+++ src/components/TriangleV2.jsx
@@ -26,6 +26,9 @@ const Triangle = () => {
         setBottomC(bottomFormC);
     
         const per = parseFloat(leftFormA) + parseFloat(rightFormB) + parseFloat(bottomFormC);
+        // if (per === NaN) {
+        //     setPerimetr('Вы забыли написать число');
+        // }
         setPerimetr(per);
         console.log('Периметр треугольника:', per);
     }
@@ -35,17 +38,19 @@ const Triangle = () => {
         <div>
         <p className='text-gray-200 text-[20px]'>Разработка и тест оконного приложения для расчета периметра треугольника</p>
         <p className="text-[25px] mb-3">Периметр треугольника равен =  <span className="text-red-400 text-[40px]">{perimeter}</span></p>
+        {/* {!isNaN(perimeter) && <p className="text-[25px] mb-3">Периметр треугольника равен =  <span className="text-red-400 text-[40px]">{perimeter}</span></p>} */}
+
         </div>
         <div className="flex bg-gray-800 w-[1000px] justify-between flex-row">
             <div className="zone-1 flex w-[500px] h-[500px] items-center justify-center">
                 <form className="w-[278px]" onSubmit={handleSubmit} action="">
                     <div className="flex flex-col mb-5">
                         <p className="mt-2 mt-4">Лево</p>
-                        <input className="rounded-xl p-2 bg-gray-600" value={leftFormA} onChange={(e) => setLeftFormA(e.target.value)} type="number" />
+                        <input required='true' className="rounded-xl p-2 bg-gray-600" value={leftFormA} onChange={(e) => setLeftFormA(e.target.value)} type="number" />
                         <p className="mt-2">Право</p>
-                        <input className="rounded-xl p-2 bg-gray-600" value={rightFormB} onChange={(e) => setRightFormB(e.target.value)} type="number" />
+                        <input required='true' className="rounded-xl p-2 bg-gray-600" value={rightFormB} onChange={(e) => setRightFormB(e.target.value)} type="number" />
                         <p className="mt-2">Нижний</p>
-                        <input className="rounded-xl p-2 bg-gray-600" value={bottomFormC} onChange={(e) => setBottomFormC(e.target.value)} type="number" />
+                        <input required='true' className="rounded-xl p-2 bg-gray-600" value={bottomFormC} onChange={(e) => setBottomFormC(e.target.value)} type="number" />
                     </div>
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Визуализировать треугольник</button>
                 </form>
